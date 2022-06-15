c474f272e2dc6a4771875ac6a96d03ceb9d4e9ff
Del Afk <97353819+Del-afk@users.noreply.github.com>
2022-06-15 13:17:12 +0430

    week one homework done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import "./styles.css";
 import "./searchbar";
 import Searchbar from "./searchbar";
 import TodayPart from "./today-part";
-import "@fortawesome/react-fontawesome";
-import "@fortawesome/free-solid-svg-icons";
-import "@fortawesome/fontawesome-svg-core";
 
 export default function App() {
   return (
